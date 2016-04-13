0e09315ea9431304f5366571efb68337d5d1cfa0
Dody Gunawinata <dodyg@silverkeytech.com>
2016-04-13 12:54:32 +0200

    Move classes to shared project so we can test it

--- TlsApp/MainActivity.cs
+++ TlsApp/MainActivity.cs
@@ -11,6 +11,7 @@ using System.Net.Http;
 using System.Text;
 using ModernHttpClient;
 using System.Net.Http.Headers;
+using Shared;
 
 namespace TlsApp
 {
@@ -40,49 +41,6 @@ namespace TlsApp
                 alert.Show();
             }
 
-        }
-    }
-
-    public class Lookup
-    {
-
-    }
-
-    public class LookupAPI
-    {
-        public async Task<Lookup> GetLookupAsync()
-        {
-            using (var client = HttpClientExtensions.Instance())
-            {
-                var payload = await client.GetJsonAsync<Lookup>("v2/lookup/").ConfigureAwait(false);
-                return payload;
-            };
-        }
-    }
-
-    public static class HttpClientExtensions
-    {
-        public static async Task<V> GetJsonAsync<V>(this HttpClient self, string url)
-        {
-            var response = await self.GetAsync(url);
-
-            var result = await response.Content.ReadAsStringAsync();
-
-            var resultPayload = JsonConvert.DeserializeObject<V>(result);
-            return resultPayload;
-        }
-
-        public static HttpClient Instance()
-        {
-            var handler = new NativeMessageHandler();
-            handler.ClientCertificateOptions = ClientCertificateOption.Automatic;
-
-            var client = new HttpClient(handler);
-            client.BaseAddress = new Uri("https://migrantfootprints.info");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            return client;
         }
     }
 }
